5d57356471a3fd71d6ce03b69971d53f2dfee28e
Chris4562022 <williamschris83@gmail.com>
2022-05-13 17:22:46 -0400

    intern passing test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,7 +3,7 @@ const Intern = require('../lib/Intern');
 const intern = new Intern('Chris', '12345', 'williamschris83@gmail.com', 'Rutgers');
 
 //make intern object
-Test('creates a new intern object', () => {
+test('creates a new intern object', () => {
     // expect(intern.name).toBe('Chris');
     // expect(intern.id).toBe('12345');
     // expect(intern.email).toBe('williamschris83@gmail.com');
@@ -13,6 +13,6 @@ Test('creates a new intern object', () => {
 
 test('test if you can get school from the getSchool()', () => {
     const intern = new Intern('Chris', '12345', 'williamschris83@gmail.com', 'Rutgers');
-    expect(intern.getSchool).toEqual(expect.stringContaining(intern.school.toString()));
+    expect(intern.school).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
