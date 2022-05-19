9014f59fcfd8d340d873a6bc9d1a8c4d6d276947
Chris4562022 <williamschris83@gmail.com>
2022-05-19 11:30:18 -0400

    updaing the intern and engineer questions

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const { Console } = require("console");
 
 
 //create a user array for user input
 
-const init = () => {
-
-    return inquirer.prompt([
+const addManager = () => {
+      return inquirer.prompt([
         {
             type: 'input',
             name: 'managername',
@@ -65,61 +65,100 @@ const init = () => {
             return false;
           }
         }
-      },
-      {
-        type: 'list',
-        name: 'teammembers',
-        message: 'Please select if you would like to add and engineer or intern to your team.',
-        choices: ['engineer', 'intern', 'I am finished building my team'],
-        validate: teammembersInput => {
-          if (teammembersInput.data === 'I am finished building my team')
-          return [];
-          else (teammembersInput.data == 'engineer')
-          console.log('Please enter your engineers Github username')
-          return = (engineerGithub);
-        }
-      },
-      {
-        type: 'input',
-        name: 'testIns',
-        message: 'Please provide testing instructions for your project. (Required)',
-        validate: testInsInput => {
-          if (testInsInput) {
-            return true;
-          } else {
-            console.log('Please provide some testing instructions for your project!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Please enter your GitHub username. (Required)',
-        validate: githubInput => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log('Please enter your github username!');
-            return false;
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email address. (Required)',
-        validate: emailInput => {
-          if (emailInput) {
-            return true;
-          } else {
-            console.log('Please enter your email address!');
-            return false;
-          }
-        }
       }
     ]);
 };
 
+const addEmployee = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'newemployee',
+      message: 'Please select who you would like to add to your team.',
+      choices: ['engineer', 'intern'],
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is this employees name?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log ('Please enter this employees name!');
+            return false;
+          }
+        }
+      },
+      {
+      type: 'input',
+        name: 'id',
+        message: 'What is this employees ID number?',
+        validate: idInput => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log ('Please enter this employees ID number!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+          name: 'email',
+          message: 'What is this employees email address?',
+          validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log ('Please enter this employees email address!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+            name: 'github',
+            message: 'What is this employees GitHub Username?',
+            when: (input) => input.newemployee === "engineer",
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log ('Please enter this employees GitHub Username!');
+                return false;
+              }
+            }
+          },
+          
+          {
+            type: 'input',
+              name: 'internschool',
+              message: 'What is the interns school?',
+              when: (input) => input.newemployee === "intern",
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log ('Please enter the interns school!');
+                return false;
+                }
+              }
+            },
+            {
+              type: 'confirm',
+              name: 'confirmadditionalteammember',
+              message: 'Would you like to add another employee to this team?',
+              default: false
+            }
+          ])
+
+
+};
+
+
+      
+
 //function call to initilize app
-init()
+addManager()
+  .then(addEmployee)
